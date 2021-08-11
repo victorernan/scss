3bae4ab6a237d04cf7f8503b087356edcf89ec3f
victorernan <git config --global user.email>
2021-08-11 20:37:12 +0200

    js file to create imagenes

---
+++ js/sneak-creative.js
@@ -0,0 +1,32 @@
+import { imagenes } from './db-fotos.js';
+
+const pics = document.querySelector('.grid');
+const fotos = imagenes();
+
+eventosListener();
+
+function eventosListener() {
+    document.addEventListener('DOMContentLoaded', () => {
+        mostrarfotos(fotos);
+    });
+}
+
+function mostrarfotos(imagenes) {
+    imagenes.forEach(iterador => {
+        //destructory is not working
+        //const { name, desc } = imagenes;
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+
+        const imagen = document.createElement('img');
+        imagen.setAttribute("src", iterador.name);
+        imagen.setAttribute("alt", iterador.desc);
+        imagen.setAttribute("width", '90%');
+
+        div.appendChild(imagen);
+        pics.appendChild(div);
+
+    });
+
+
+}
